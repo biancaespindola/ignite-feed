1e57ed5d3394ec5ffcf41641e4b4b8a62464a579
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-04-30 21:19:41 +0200

    adicionando sessao de comentarios

---
+++ src/components/Comment.tsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+	return (
+		<div className={styles.comment}>
+			<img
+				className={styles.avatar}
+				src="https://github.com/biancaespindola.png"
+				alt="profile image user"
+			/>
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAnTime}>
+							<strong>Bianca Espindola</strong>
+							<time
+								title="20 April 2023 at 12:00 pm"
+								dateTime="2023-04-20 12:00:00"
+							>
+								About an hour ago
+							</time>
+						</div>
+						<button title="Delete comment">
+							<Trash size={20} />
+						</button>
+					</header>
+					<p>Congratulations Bianca, Good job!</p>
+				</div>
+				<footer>
+					<button>
+						<ThumbsUp />
+						Aplaudir <span>20</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
